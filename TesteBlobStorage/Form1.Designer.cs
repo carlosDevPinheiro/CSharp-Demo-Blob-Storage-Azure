7a47de7d226c9df158190fa153286ea8d60f0f3a
Antonio Carlos Pinheiro <prog.antoniocarlos@outlook.com>
2017-07-08 16:39:44 -0400

    Demo Funcionando perfeitamente

--- TesteBlobStorage/Form1.Designer.cs
+++ TesteBlobStorage/Form1.Designer.cs
@@ -28,12 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lstCarregados = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNomeBlob = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblContatiner = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // lstCarregados
+            // 
+            this.lstCarregados.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lstCarregados.Font = new System.Drawing.Font("Arial Narrow", 13.2F);
+            this.lstCarregados.FormattingEnabled = true;
+            this.lstCarregados.ItemHeight = 22;
+            this.lstCarregados.Location = new System.Drawing.Point(49, 241);
+            this.lstCarregados.Margin = new System.Windows.Forms.Padding(2);
+            this.lstCarregados.Name = "lstCarregados";
+            this.lstCarregados.Size = new System.Drawing.Size(388, 134);
+            this.lstCarregados.TabIndex = 17;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(45, 214);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(204, 22);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Blobs já carregados:";
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluir.Location = new System.Drawing.Point(324, 162);
+            this.btnExcluir.Margin = new System.Windows.Forms.Padding(2);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(112, 32);
+            this.btnExcluir.TabIndex = 15;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDownload.Location = new System.Drawing.Point(188, 162);
+            this.btnDownload.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(112, 32);
+            this.btnDownload.TabIndex = 14;
+            this.btnDownload.Text = "Dowload";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpload.Location = new System.Drawing.Point(49, 162);
+            this.btnUpload.Margin = new System.Windows.Forms.Padding(2);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(112, 32);
+            this.btnUpload.TabIndex = 13;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(45, 89);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 22);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Blob:";
+            // 
+            // txtNomeBlob
+            // 
+            this.txtNomeBlob.Font = new System.Drawing.Font("Arial Narrow", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNomeBlob.Location = new System.Drawing.Point(49, 115);
+            this.txtNomeBlob.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNomeBlob.MaxLength = 1024;
+            this.txtNomeBlob.Name = "txtNomeBlob";
+            this.txtNomeBlob.Size = new System.Drawing.Size(389, 28);
+            this.txtNomeBlob.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 13.8F);
+            this.label2.Location = new System.Drawing.Point(149, 40);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 22);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "testecontainer";
+            // 
+            // lblContatiner
+            // 
+            this.lblContatiner.AutoSize = true;
+            this.lblContatiner.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContatiner.Location = new System.Drawing.Point(45, 40);
+            this.lblContatiner.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblContatiner.Name = "lblContatiner";
+            this.lblContatiner.Size = new System.Drawing.Size(108, 22);
+            this.lblContatiner.TabIndex = 9;
+            this.lblContatiner.Text = "Container:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 462);
+            this.Controls.Add(this.lstCarregados);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNomeBlob);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblContatiner);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstCarregados;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNomeBlob;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblContatiner;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- TesteBlobStorage/Form1.cs
+++ TesteBlobStorage/Form1.cs
@@ -12,9 +12,68 @@ namespace TesteBlobStorage
 {
     public partial class Form1 : Form
     {
+        private const string CONTAINER = "testecontainer";
         public Form1()
         {
             InitializeComponent();
+
+            BlobStorageHelper.CheckContainer(CONTAINER);
+            AtualizarListagemBlobs();
+        }
+
+        private bool NomeBlobValido()
+        {
+            bool valido = !String.IsNullOrWhiteSpace(txtNomeBlob.Text);
+            if (!valido)
+                MessageBox.Show("Preencha o nome do blob!");
+
+            return valido;
+        }
+
+        private void AtualizarListagemBlobs()
+        {
+            lstCarregados.Items.Clear();
+            lstCarregados.Items.AddRange(
+                BlobStorageHelper.ListBlockBlobs(CONTAINER)
+                    .ToArray());
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            if (!NomeBlobValido())
+                return;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                BlobStorageHelper.UploadBlockBlob(
+                    CONTAINER, txtNomeBlob.Text, openFileDialog1.FileName);
+                AtualizarListagemBlobs();
+                MessageBox.Show("Blob criado com sucesso!");
+            }
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            if (!NomeBlobValido())
+                return;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                BlobStorageHelper.DownloadBlockBlob(
+                    CONTAINER, txtNomeBlob.Text, saveFileDialog1.FileName);
+                AtualizarListagemBlobs();
+                MessageBox.Show("Blob baixado com sucesso!");
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if (!NomeBlobValido())
+                return;
+
+            BlobStorageHelper.DeleteBlockBlob(CONTAINER, txtNomeBlob.Text);
+            AtualizarListagemBlobs();
+            MessageBox.Show("Blob removido com sucesso!");
         }
     }
 }
